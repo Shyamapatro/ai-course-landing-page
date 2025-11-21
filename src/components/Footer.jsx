7d1617b91c1fa24c25d256a0c62917f0c5161818
shyamapatro <shyama.patro@gmail.com>
2025-11-21 10:45:51 +0530

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
 
                 <div className="border-t border-white/10 pt-8 flex flex-col md:flex-row justify-between items-center">
-                    <p className="text-gray-400 text-sm">
+                    <p className="text-gray-300 text-sm">
                         &copy; {new Date().getFullYear()} VeerAI. All rights reserved.
                     </p>
                     <div className="flex space-x-6 mt-4 md:mt-0">
